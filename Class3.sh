05653f50472563e0edadd285ed888c337df559eb
Nithrone <kadir.dokur@bil.omu.edu.tr>
2024-04-30 17:07:55 +0300

    new file:   Class3.sh 	new file:   deneme.sh 	modified:   myFirstscript.sh

---
+++ Class3.sh
@@ -0,0 +1,12 @@
+
+#It is not required to include double quote but is required to provide better code visibility
+echo "Hello World"
+echo Hello World
+
+# Both echoes will yield the same result
+
+# ---------------- Variable Declaration ----------------
+#In order to be able to reference a variable the dollar sign($) must be given before the variable
+myname="piotr"
+echo $myname
+
