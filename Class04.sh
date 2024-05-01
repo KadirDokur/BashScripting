de47c4327788ca4b3e396a06168057936c3e5f97
Nithrone <kadir.dokur@bil.omu.edu.tr>
2024-05-01 08:09:14 +0300

    modified:   Class04.sh

---
+++ Class04.sh
@@ -0,0 +1,16 @@
+#-----------------------Basic Math---------------------
+
+# The evaluate expression is required before doing any math operation
+expr 30 + 10
+expr 30 - 20
+expr 30 / 10
+
+# 30 + 10 is a no valid entry for such operation. Feel free to try out.
+
+# Multiplication is an exception as the asterisk(*) symbol is a wildcard in Linux.
+# Click on the link to see more 
+# "https://pressbooks.senecacollege.ca/uli101/chapter/wildcard-selection-in-bash/#:~:text=In%20the%20Bash%20command%20line,current%20directory%20that%20have%20the%20%22."
+
+# Backslash (\) is the way to overcome this.
+expr 30 \* 10
+
