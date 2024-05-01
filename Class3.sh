7aefbbca383380d1b521a5b9db9750513e593b13
Nithrone <kadir.dokur@bil.omu.edu.tr>
2024-05-01 07:58:43 +0300

    modified:   Class3.sh 	modified:   deneme.sh

--- Class3.sh
+++ Class3.sh
@@ -13,3 +13,19 @@ myname="piotr"
 echo $myname
 
 # Variables are only stored within the session. Meaning that if the terminal is closed any previously written variables will also be wiped from the memory.
+name="piotr"
+age="25"
+echo "My name is $name"
+echo "I am $age years old"
+
+# Using the output of a command within a Bash Script
+name="Piotr Knopp"
+now=$(date)
+
+echo "Hello $name"
+
+echo "The system time and date is:"
+echo $now
+
+# The code below is valid in Linux environment but not in windows. So it wil yield empty output if tried on windows machine. Tested on Kali Linux and it works fine
+echo "Your username is: $USER"
