5f160ff5ce1738fb967bb10db46d555fb75f95fa
Nithrone <kadir.dokur@bil.omu.edu.tr>
2024-05-11 06:23:24 +0300

    modified:   Class05/Check_File_Existence.sh 	deleted:    Class05/myfile

--- Class05/Check_File_Existence.sh
+++ Class05/Check_File_Existence.sh
@@ -1,18 +1,37 @@
-if [ -f ~/myfile ]
+# This program will check if a program is within a directory
+
+# The working directory will be changed to this folder. Used "pwd" to confirm
+cd /c/Users/Nith/Desktop/BashScripting/Class05
+pwd
+
+# The code is self explanatory here. "-f" stands for the file parameter
+if [ -f /c/Users/Nith/Desktop/BashScripting/Class05/myfile ]
 then
     echo "The file exists."
 else
     echo "The file does not exist."
 fi
 
-cd Class05
-
+# Creating a temporary file named "myfile" because it is what is being searched.
+echo "The file is being created as it was not found..."
 touch myfile
+echo "The file has been created"
 
-if [ -f ~/myfile ]
+if [ -f /c/Users/Nith/Desktop/BashScripting/Class05/myfile ]
 then
     echo "The file exists."
 else
     echo "The file does not exist."
 fi
 
+# Removing the file to confirm everything is working as intended
+echo "Removing the file..."
+rm myfile
+echo "The file has been deleted"
+
+if [ -f /c/Users/Nith/Desktop/BashScripting/Class05/myfile ]
+then
+    echo "The file exists."
+else
+    echo "The file does not exist."
+fi
