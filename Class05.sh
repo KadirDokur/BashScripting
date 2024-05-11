6e0f9817da46b8bf5da6952c0cb22f54a53dffb0
Nithrone <kadir.dokur@bil.omu.edu.tr>
2024-05-11 05:52:18 +0300

    modified:   Class05.sh

--- Class05.sh
+++ Class05.sh
@@ -1,7 +1,33 @@
 #------------If Statement---------
 
-mynum="47"
+mynum=47
 if [ $mynum -eq 47 ]
 then
     echo "The condition is true!"
 fi
+
+# Unconventional way of using if/else condition
+if [ $mynum -eq 300 ]
+then
+    echo "The variable does not equal 200."
+fi
+
+# Better way
+if [ ! $mynum -eq 200 ]
+then
+    echo "The condition is true"
+else
+    echo "The variable does not equal 200."
+fi
+
+# Even better without the exclamation mark
+
+if [  $mynum -ne 200 ]
+then
+    echo "The condition is true"
+else
+    echo "The variable does not equal 200."
+fi
+
+# Follow the link for more comparison operators --> https://tldp.org/LDP/abs/html/comparison-ops.html
+
