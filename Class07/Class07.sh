ad00b163b0701ffc3afd3f68e4c58c290cbf40ee
Nithrone <kadir.dokur@bil.omu.edu.tr>
2024-05-13 09:00:59 +0300

    new file:   Class07/Class07.sh

---
+++ Class07/Class07.sh
@@ -0,0 +1,26 @@
+#------------While Loops ---------
+
+mynum=1
+
+while [ $mynum -le 10 ]
+do
+    echo $mynum 
+    #incrementing our variable by 1. Paranthesis are important
+    mynum=$(( $mynum +1 ))
+
+    #Putting 0.2 seconds of delay each time it prints
+    sleep 0.2
+done
+
+#Another Example
+number=1000
+counter=0
+
+while [ ! $number -eq 1 ]
+do
+    echo $number
+    (( number /= 10 ))
+    ((counter++))
+    sleep 0.1
+done
+echo "Amount of division it took:$counter"
