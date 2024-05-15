057de36f3b058af3f33dfe73caee071c7584b029
Nithrone <kadir.dokur@bil.omu.edu.tr>
2024-05-15 20:07:18 +0300

    modified:   Class05/Check_File_Existence.sh

--- Class05/Check_File_Existence.sh
+++ Class05/Check_File_Existence.sh
@@ -15,7 +15,7 @@ fi
 # Creating a temporary file named "myfile" because it is what is being searched.
 echo "The file is being created as it was not found..."
 touch myfile
-echo "The file has been created"
+echo "The file has been created."
 
 if [ -f /c/Users/Nith/Desktop/BashScripting/Class05/myfile ]
 then
@@ -27,7 +27,7 @@ fi
 # Removing the file to confirm everything is working as intended
 echo "Removing the file..."
 rm myfile
-echo "The file has been deleted"
+echo "The file has been deleted."
 
 if [ -f /c/Users/Nith/Desktop/BashScripting/Class05/myfile ]
 then
